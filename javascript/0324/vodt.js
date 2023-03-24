4decf75845ea2532082f5a5f46e9fe743f46adf3
karta9630 <4909044@gmail.com>
2023-03-24 12:10:39 +0800

    123

--- javascript/0324/vodt.js
+++ javascript/0324/vodt.js
@@ -1,11 +1,11 @@
-function vdot(a,b){
-    let r=[],n=0
-    for(let i=0;i<a.length;i++){
-        r[i]=(a[i]*b[i]);
-       n=n+r[i];
+function vdot(a, b) {
+    let r = [], n = 0
+    for (let i = 0; i < a.length; i++) {
+        r[i] = (a[i] * b[i]);
+        n = n + r[i];
     }
 
     return n
 }
-    var a=[2,4,6,7],b=[8,6,4,3];
-    console.log(vdot(a,b));
+var a = [2, 4, 6, 7], b = [8, 6, 4, 3];
+console.log(vdot(a, b));
