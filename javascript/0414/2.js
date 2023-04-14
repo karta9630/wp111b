548b55d5ca15ff1abca7df90450bfcb611f97347
karta9630 <4909044@gmail.com>
2023-04-14 11:33:52 +0800

    123

---
+++ javascript/0414/2.js
@@ -0,0 +1,17 @@
+
+function count(list) {
+    var x = {}
+    for (var i of list) {
+        if (i in x) {
+            x[i]++
+        }
+        else {
+            x[i] = 1
+        }
+    }
+    return x
+}
+
+var list = ['a', 'dog', 'chase', 'a', 'cat']
+
+console.log(count(list))
