10931b0308487e6083e404e16626e868e7ae5dad
karta9630 <4909044@gmail.com>
2023-04-28 11:33:50 +0800

    123

--- javascript/0428/topbar.js
+++ javascript/0428/topbar.js
@@ -16,13 +16,24 @@ function myFunction() {
   let main = document.querySelector('#main')
   let pages={
     '#login':`
+    <h1>登入</h1>
     <input type="text" placeholder="帳號"/><br/>
-    <input type="text" placeholder="密碼"/><br/>`,
+    <input type="text" placeholder="密碼"/><br/>
+    <a href="#home">確認</a>
+    `,
     '#home':`
     <h1>首頁</h1>
     <p>本網頁可以按功能表去切換畫面</p> `, 
     '#logout':`
-    <button >登出</button> 
+    <a href="#home">登出</a> 
+    `,
+    '#signup':`
+    <h1>註冊</h1>
+    <input type="text" placeholder="電子郵件"/><br/>
+    <input type="text" placeholder="帳號"/><br/>
+    <input type="text" placeholder="密碼"/><br/>
+    <input type="text" placeholder="電號號碼"/><br/>
+    <a href="#login">送出</a>
     `,
   }
   
